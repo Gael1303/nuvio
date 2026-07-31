ff76feb92a9b788296ee903c16a9fdb681893833
Gael.Add <109701922+Gael1303@users.noreply.github.com>
2026-07-30 23:18:16 -0300

    Create test-serie-starck.js

---
+++ test-serie-starck.js
@@ -0,0 +1,19 @@
+// Testa série no StarckFilmes. Roda com: node test-serie-starck.js <tmdbId> <season> <episode>
+// Exemplo: node test-serie-starck.js 1396 1 1  (Breaking Bad S1E1)
+const { getStreams } = require('./starckfilmes-movies.js');
+
+const tmdbId = process.argv[2] || '1396';
+const season = parseInt(process.argv[3] || '1', 10);
+const episode = parseInt(process.argv[4] || '1', 10);
+
+console.log('Testando: tmdbId=' + tmdbId + ' season=' + season + ' episode=' + episode);
+
+getStreams(tmdbId, 'tv', season, episode)
+  .then((streams) => {
+    console.log('\n✅ ' + streams.length + ' resultado(s) encontrado(s):\n');
+    console.log(JSON.stringify(streams, null, 2));
+  })
+  .catch((err) => {
+    console.error('❌ Erro fatal no teste:', err);
+    process.exit(1);
+  });
